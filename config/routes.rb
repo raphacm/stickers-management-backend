45278a116844a5f34990d51992fba8bbfb76d1ae
Raphael Costa <raphacm@gmail.com>
2019-09-10 22:21:21 -0300

    master-03: initiating collection model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :collectors, only: %i[show create destroy]
+      resources :collectors, only: %i[show create destroy] do
+        resources :collections, only: %i[index show create destroy]
+      end
       resources :albums, only: %i[index show create update]
     end
   end
